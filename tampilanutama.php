ea53ab9702785fe9cee65aa812f7993b4b4f39ab
Robinhutts <113872730+Robinhutts@users.noreply.github.com>
2023-01-17 11:38:39 +0700

    Add files via upload

---
+++ tampilanutama.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="tampilanutama.css">
+    <title></title>
+</head>
+
+<body>
+    
+    <div class="banner-text">
+        <ul>
+            <li>Tentang Kami</li>
+            
+            
+        </ul>
+        <h2> Welcome To ELY Store </h2>
+        <h4> ELY store adalah toko pakaian Online, Berbagai topi, baju, jaket, celana,</h4>
+        <h5> berbagai macam aksesoris mulai dari atas sampai bawah kaki dan juga menyediakan semua jenis makanan.</h5>
+
+            
+
+              
+                <a href="loginadmin.php" class="btn btn-primary mb-2 mr-2 nuxt-link-exact-active nuxt-link-active"> Log In Admin</a> 
+                <a href="login.php" class="btn btn-primary mb-2 mr-2 nuxt-link-exact-active nuxt-link-active"> Log In Users</a> 
+            
+    </div>
+    
+
+    <div class="animation">
+        <ul class="box-area">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+
+</body>
+</html>
+
+
